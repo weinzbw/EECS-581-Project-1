bda70b0d74f26055cfb796bf776a5ba62df3e4c0
weinzbw <130248057+weinzbw@users.noreply.github.com>
2024-09-15 15:29:51 -0500

    Defined more of the turn() function

--- game.py
+++ game.py
@@ -24,13 +24,14 @@ class Game:
             if square == 0:
                 player.ships[x][y] = "M"
                 return "missed!"
-            # If the sqaure has been hit before, raise IndexError
+            # If the square has been hit before, raise IndexError
             elif square == 'X' or 'S' or "M":
                 raise IndexError("Index picked before")
             # Otherwise, it is a hit
             else:
                 # Checks if the ship is sunk
                 if player.is_sunk(square):
+                    player.num_ships -= 1
                     # Checks if all ships are sunk
                     if player.is_all_sunk():
                         player.ships[x][y] = "S"
@@ -46,6 +47,17 @@ class Game:
     
     def turn(self,player,target):
         x = input("Type the X-coordinate (A-J) of your target:")
-        y = input("Type the Y-coordinate (1-10) of your target: ")
-        self.shot(target,x,y)
-        
+        y = int(input("Type the Y-coordinate (1-10) of your target: "))
+        # Changes the string to an integer
+        x = ord(x.upper()) - ord('A')
+        #shot() method can raise an error with invalid input
+        try:
+            shot = self.shot(target,x,y)
+            print(shot)
+            # Takes another turn if it is a hit
+            if shot == "You hit a ship!":
+                player.print_board(player.shots)
+                self.turn(player, target)
+        # Gives another chance to input proper coords if an error is raised
+        except:
+            self.turn(player.target)
